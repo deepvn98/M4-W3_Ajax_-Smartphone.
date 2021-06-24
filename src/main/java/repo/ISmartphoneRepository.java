1d791a9ac97d9cde3bf6d5bbe18faa3fbcdad5a4
deepvn98 <deepvn988@gmail.com>
2021-06-24 19:31:15 +0700

    chưa hiểu jquerry

---
+++ src/main/java/repo/ISmartphoneRepository.java
@@ -0,0 +1,9 @@
+package repo;
+
+import model.Smartphone;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISmartphoneRepository extends CrudRepository<Smartphone, Long> {
+}
